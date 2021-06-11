green7ea/home

Initial commit of hikaru's configs

.emacs.d/elisp/lang-rust.el
@@ -1,0 +1,13 @@
+(use-package rustic
+  :bind (:map rustic-mode-map
+              ("M-RET" . lsp-execute-code-action)
+              ([f1] . lsp-describe-thing-at-point)
+              ([f2] . lsp-find-references)
+              ([f5] . cargo-process-check)
+              ("C-<f5>" . cargo-process-build)
+              ([f6] . rust-format-buffer))
+
+  :mode
+  ("\\.rs$" . rustic-mode))
+
+(provide 'lang-rust)

green7ea/dotfiles

.emacs.d/elisp/lang-rust.el
@@ -1,0 +1,13 @@
+(use-package rustic
+  :bind (:map rustic-mode-map
+              ("M-RET" . lsp-execute-code-action)
+              ([f1] . lsp-describe-thing-at-point)
+              ([f2] . lsp-find-references)
+              ([f5] . rustic-cargo-check)
+              ("C-<f5>" . rustic-cargo-build)
+              ([f6] . rustic-format-buffer))
+
+  :mode
+  ("\\.rs$" . rustic-mode))
+
+(provide 'lang-rust)