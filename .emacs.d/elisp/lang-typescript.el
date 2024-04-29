776691a6090c9466fc704da6d9edd8120a2f93d9
Emmanuel Boudreault <manu@twimberg.at>
2024-04-29 11:42:07 +0200

    Update typescript

--- .emacs.d/elisp/lang-typescript.el
+++ .emacs.d/elisp/lang-typescript.el
@@ -1,17 +1,14 @@
-(define-derived-mode typescriptreact-mode typescript-ts-mode "TypescriptReact"
-  "A major mode for tsx.")
-
-(use-package typescript-ts-mode
+(use-package tide
   :mode (("\\.ts$" . typescript-ts-mode)
-         ("\\.tsx$" . typescriptreact-mode))
-  :hook ((typescript-ts-mode . eglot-ensure))
-  :bind (:map typescript-ts-mode-map
-              ("M-RET" . eglot-code-action)
-              ; ([f1] . lsp-find-reference)
-              ([f2] . eglot-rename)
-              ; ([f5] . lsp-treemacs-error-list)
-              ([f6] . eglot-format-buffer))
-  :init
-  (setq typescript-indent-level 2))
+         ("\\.tsx$" . tsx-ts-mode))
+  :init (keymap-global-unset "M-<return>")
+  :after (company flycheck)
+  :hook ((typescript-ts-mode . tide-setup)
+         (tsx-ts-mode . tide-setup)
+         (typescript-ts-mode . tide-hl-identifier-mode))
+  :bind (:map tide-mode-map
+              ("M-RET" . tide-fix)
+              ([f1] . tide-references)
+              ([f2] . tide-rename-symbol)))
 
 (provide 'lang-typescript)
