ce7242653a249f63b89e875333ea146f8a0ccb83
Emmanuel Boudreault <manu@twimberg.at>
2024-09-20 11:31:37 +0200

    Fix wezterm resize not always workind

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,7 +11,7 @@ config.color_scheme = 'Tiniri Dark'
 config.font = wezterm.font 'Fira Code'
 config.font_size = 14.0
 
-config.window_decorations = 'NONE'
+config.window_decorations = 'RESIZE'
 
 config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 
@@ -20,6 +20,6 @@ config.hide_tab_bar_if_only_one_tab = true
 config.default_cursor_style = "SteadyUnderline"
 
 config.initial_rows = 32
-config.initial_cols = 128
+config.initial_cols = 80
 
 return config
