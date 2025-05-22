4d3a6befc59a442d7a9476b4f060c9c5eca740d4
Emmanuel Boudreault <manu@twimberg.at>
2025-05-22 18:04:08 +0200

    Switch from vterm to mistty

--- .emacs.d/elisp/base-shell.el
+++ .emacs.d/elisp/base-shell.el
@@ -1,4 +1,7 @@
-(use-package vterm
-  :bind ([f11] . vterm))
+(use-package mistty
+  :bind (([f11] . mistty)
+         :map mistty-prompt-map
+         ("M-<up>" . mistty-send-key)
+         ("M-<down>" . mistty-send-key)))
 
 (provide 'base-shell)
