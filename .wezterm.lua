2318c8ac2a05b332f04345856263944e272cc26b
Emmanuel Boudreault <manu@twimberg.at>
2025-05-28 16:03:43 +0200

    Use monaspace in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,12 +8,14 @@ end
 config.color_scheme_dirs = { '/home/manu/.wezterm/themes' }
 config.color_scheme = 'Tiniri Dark'
 
-config.font = wezterm.font 'Fira Code'
+config.font = wezterm.font({
+  family='Monaspace Neon',
+  harfbuzz_features={ 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08' },
+})
+
 config.font_size = 14.0
 
 config.window_decorations = 'RESIZE'
-
-config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 
 config.hide_tab_bar_if_only_one_tab = true
 
