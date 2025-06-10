aa54b23f46256a4f57256e88ec3a9db3a7c94f17
Emmanuel Boudreault <manu@twimberg.at>
2025-06-10 10:55:46 +0200

    Make emacs terminal friendly

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,8 +1,11 @@
 (setq package-enable-at-startup nil)
 (setq site-run-file nil)
 (menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+
+(when (display-graphic-p)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1))
+
 (unless (and (display-graphic-p) (eq system-type 'darwin))
   (push '(menu-bar-lines . 0) default-frame-alist))
 (push '(tool-bar-lines . 0) default-frame-alist)
