1644639c335b10edb1640643b861fcef5bc4b0b1
Emmanuel Boudreault <manu@twimberg.at>
2025-06-19 21:03:19 +0200

    Use the langtool exec directly instead of server

--- .emacs.d/elisp/base-langtool.el
+++ .emacs.d/elisp/base-langtool.el
@@ -1,19 +1,5 @@
-(use-package languagetool
-  :ensure t
-  :defer t
-  :bind (:map languagetool-server-mode
-              ("M-RET" . languagetool-correct-at-point))
-  :commands (languagetool-check
-             languagetool-clear-suggestions
-             languagetool-correct-at-point
-             languagetool-correct-buffer
-             languagetool-set-language
-             languagetool-server-mode
-             languagetool-server-start
-             languagetool-server-stop)
+(use-package langtool
   :config
-  (setq languagetool-java-arguments '("-Dfile.encoding=UTF-8")
-        languagetool-server-url "localhost"
-        languagetool-server-port 8081))
+  (setq langtool-bin "/usr/bin/languagetool"))
 
 (provide 'base-langtool)
