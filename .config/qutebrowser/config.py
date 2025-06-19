d84db61a68d03c8ea2a7141eb8058d15ec259b2a
Emmanuel Boudreault <manu@twimberg.at>
2025-06-19 21:02:05 +0200

    Update qutebrowser config to hide tabs

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,20 +1,24 @@
 c.content.javascript.enabled = False
 c.content.blocking.enabled = True
 
-c.editor.command = ['kitty', 'emacs', '{}']
+c.editor.command = ['emacs', '{}']
 c.url.start_pages = 'https://search.brave.com/'
 c.url.default_page = 'https://search.brave.com/'
 c.url.searchengines = {'DEFAULT': 'https://search.brave.com/search?q={}'}
 
 c.hints.chars = 'arstneioqwfpuy'
+
 c.tabs.background = True
 c.tabs.last_close = 'close'
+c.tabs.show = 'switching'
+c.tabs.show_switching_delay = 4000
+
 c.zoom.default = '125%'
 
 c.fonts.default_size = '15pt'
 
 config.bind('m', 'spawn mpv {url}')
-config.bind('M', 'spawn kitty video {url}')
+config.bind('M', 'spawn wezterm start video {url}')
 
 config.bind('<Alt-x>', 'cmd-set-text :')
 config.bind('<Ctrl-s>', 'cmd-set-text :search')
@@ -36,5 +40,7 @@ config.bind('gO', 'cmd-set-text :open -t -r ')
 
 config.bind('r', 'spawn --userscript readability')
 config.bind('R', 'spawn remarkable {url} {title}')
+
+
 
 config.load_autoconfig(False)
