e22c5a7b498afae73b0a65ab0fe83d2a40be7742
Emmanuel Boudreault <manu@twimberg.at>
2025-10-14 13:14:46 +0200

    Use spaceious padding to make emacs look nicer

--- .emacs.d/elisp/base-theme.el
+++ .emacs.d/elisp/base-theme.el
@@ -67,19 +67,10 @@
   :init
   (doom-modeline-mode 1))
 
-(setq default-frame-alist
-      (append (list
-	             '(min-height . 1)
-               '(height     . 45)
-	             '(min-width  . 1)
-               '(width      . 81)
-               '(internal-border-width . 12)
-               '(left-fringe    . 0)
-               '(right-fringe   . 0)
-               '(menu-bar-lines . 0))))
-
-(setq-default left-margin-width 1
-              right-margin-width 1)
+(use-package spacious-padding
+  :ensure t
+  :init
+  (spacious-padding-mode 1))
 
 (provide 'base-theme)
 ;;; base-theme.el ends here
